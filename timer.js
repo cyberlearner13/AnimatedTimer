10eb02eebc5ce1c3db9c0c6fb394030ef9c835d5
cyberlearner13 <arkadeepmukherjee2012@gmail.com>
2021-02-15 22:41:06 +0530

    code refactor

---
+++ timer.js
@@ -0,0 +1,43 @@
+class Timer {
+  constructor(durationInput, startButton, pauseButton, callbacks) {
+    this.durationInput = durationInput;
+    this.startButton = startButton;
+    this.pauseButton = pauseButton;
+    if (callbacks) {
+      this.onStart = callbacks.onStart;
+      this.onTick = callbacks.onTick;
+      this.onComplete = callbacks.onComplete;
+    }
+    this.startButton.addEventListener('click', this.start);
+    this.pauseButton.addEventListener('click', this.pause);
+  }
+
+  start = () => {
+    this.onStart && this.onStart();
+    this.tick();
+    // Use setInterval for calling tick every second
+    this.interval = setInterval(this.tick, 1000);
+  };
+
+  pause = () => {
+    clearInterval(this.interval);
+  };
+
+  tick = () => {
+    if (this.timeRemaining <= 0) {
+      this.pause();
+      this.onComplete && this.onComplete();
+    } else {
+      this.timeRemaining = this.timeRemaining - 1;
+      this.onTick && this.onTick();
+    }
+  };
+
+  get timeRemaining() {
+    return +this.durationInput.value;
+  }
+
+  set timeRemaining(time) {
+    this.durationInput.value = time;
+  }
+}
